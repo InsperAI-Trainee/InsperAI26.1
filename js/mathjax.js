92e82fd0c639505a04d20e11f47ae511780be66e
thomaskmk <thomaskmk1@gmail.com>
2026-04-06 20:32:00 -0300

    Deployed fb4c7a8 with MkDocs version: 1.6.1

--- js/mathjax.js
+++ js/mathjax.js
@@ -2,32 +2,15 @@ window.MathJax = {
   tex: {
     inlineMath: [["\\(", "\\)"]],
     displayMath: [["\\[", "\\]"]],
-    processEscapes: true,
-    processEnvironments: true
+    processEscapes: false,
+    processEnvironments: false
   },
   options: {
-    skipHtmlTags: ["script", "noscript", "style", "textarea", "pre", "code"],
     ignoreHtmlClass: ".*|",
     processHtmlClass: "arithmatex"
   }
 };
 
-function typesetMath() {
-  if (!window.MathJax || typeof window.MathJax.typesetPromise !== "function") {
-    return;
-  }
-
-  if (typeof window.MathJax.typesetClear === "function") {
-    window.MathJax.typesetClear();
-  }
-
-  window.MathJax.typesetPromise();
-}
-
-window.addEventListener("load", typesetMath);
-
-if (typeof document$ !== "undefined" && typeof document$.subscribe === "function") {
-  document$.subscribe(() => {
-    typesetMath();
-  });
-}
+document$.subscribe(() => {
+  MathJax.typesetPromise();
+});
